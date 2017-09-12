ad9dd3e3b3cadb9df0de79277648feeba52af7b6
孙文轲 <sunwenke2009@126.com>
2017-09-12 10:25:01 +0800

    add simple_form and Crud

--- db/migrate/20170912021129_create_movies.rb
+++ db/migrate/20170912021129_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.text :decription
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912014816) do
+ActiveRecord::Schema.define(version: 20170912021129) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
-    t.text     "decription"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
